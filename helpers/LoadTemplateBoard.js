2ee15cc91e9b4805e379f673ff278034214672f2
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-06-25 21:20:32 +0200

    fixed lining

---
+++ helpers/LoadTemplateBoard.js
@@ -0,0 +1,18 @@
+module.exports = {
+    LoadTemplateBoard = (newGameBoard) => {
+        let newTrueTiles = {}
+        newGameBoard.forEach((gameRow, i) => {
+            gameRow.forEach((gameTile, j) => {
+                if (gameTile) {
+                    newTrueTiles[`${i}, ${j}`] = [i, j]
+                }
+            })
+        })
+        gameBoard = newGameBoard
+        trueTiles = newTrueTiles
+        Object.values(trueTiles).forEach(array => {
+            document.getElementById(`T${array[0]},${array[1]}`).classList.toggle("hidden")
+            document.getElementById(`F${array[0]},${array[1]}`).classList.toggle("hidden")
+        })
+    }
+}
